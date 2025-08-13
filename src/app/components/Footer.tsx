f61d20e0c5a10702680fec2a385bd03b654fb641
Prasanna Patil <prasannagoudasp12@gmail.com>
2025-08-13 08:06:43 +0000

    As attached in the image, I want Pink Panther to walk on the footer line

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer: FC = () => {
   return (
     <footer className="relative py-8 bg-background/50 border-t border-border mt-auto overflow-x-hidden">
       <WalkingCharacter />
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center pt-12">
         <div className="flex justify-center mb-4">
           <ContactLinks />
         </div>
