4f02e652de6456c3af7c809966e79b6839f062f5
Prasanna Patil <prasannagoudasp12@gmail.com>
2025-08-13 08:10:34 +0000

    I don't want any pink panther animation, remove everything and bring bac

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,13 +1,11 @@
 import type { FC } from 'react';
 import ContactLinks from './ContactLinks';
-import WalkingCharacter from './WalkingCharacter';
 
 const Footer: FC = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="relative py-8 bg-background/50 border-t border-border mt-auto overflow-x-hidden">
-      <WalkingCharacter />
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center pt-12">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center">
         <div className="flex justify-center mb-4">
           <ContactLinks />
         </div>
